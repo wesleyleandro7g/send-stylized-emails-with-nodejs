c0e52b18f1d98b3f7713d6e392910550f181cfc7
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2022-04-01 12:41:26 -0300

    configured nodemailer

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import http from "http";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
+
+import transporter from "./src/config/smtp";
 
 const app = express();
 
@@ -15,11 +18,19 @@ app.get("/", (req: Request, res: Response) => {
   return res.send("SERVER IS RUNNING!");
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else if (success) {
+    console.log("smtp service connected!");
+  }
+});
+
 const server = http.createServer(app);
 
 const PORT = +process.env.PORT || 3333;
 const HOST = "0.0.0.0";
 
-console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+console.log(`server running on port ${PORT}`);
 
 server.listen(PORT, HOST);
---
+++ src/config/smtp.ts
@@ -0,0 +1,16 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: +process.env.MAIL_PORT,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+export default transporter;
