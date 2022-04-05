0f2fa6825619fa3b5daeec5f84c19c843c631ef7
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2022-04-05 12:22:51 -0300

    Updated styles

--- index.ts
+++ index.ts
@@ -9,7 +9,6 @@ import dotenv from "dotenv";
 
 import { MailsControllers } from "./src/controllers/MailsControllers";
 import { InternalError } from "./src/config/generateError";
-import transporter from "./src/config/smtp";
 
 const mailsController = new MailsControllers();
 
@@ -20,13 +19,13 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.set("views", path.join(__dirname, "src", "layouts"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/img", express.static(path.resolve(__dirname, "img")));
 
 app.get("/", (req: Request, res: Response) => {
-  res.render("layout1");
+  res.render("home");
 });
 
 app.get("/signUpRequest", (req: Request, res: Response) => {
--- src/controllers/MailsControllers.ts
+++ src/controllers/MailsControllers.ts
@@ -15,36 +15,6 @@ class MailsControllers {
     });
   }
 
-  /*   async sendSimpleMailsTemplate(req: Request, res: Response) {
-    ejs.renderFile(
-      path.resolve("src", "layouts", "layout1.ejs"),
-      function (err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          var mainOptions = {
-            from: "izaquedione@gmail.com",
-            to: "izaquenunes560@gmail.com",
-            subject: "Test mails template",
-            html: data,
-          };
-
-          transporter.sendMail(mainOptions, function (err, info) {
-            if (err) {
-              res.json({
-                msg: "fail",
-              });
-            } else {
-              res.json({
-                msg: "success",
-              });
-            }
-          });
-        }
-      }
-    );
-  } */
-
   async sendSimpleMailsTemplate(req: Request, res: Response) {
     const emailTemplate = fs.readFileSync(
       path.resolve("src/layouts/template1.hbs"),
@@ -54,12 +24,12 @@ class MailsControllers {
     const template = hbs.compile(emailTemplate);
 
     const html = template({
-      companyName: "Extra Bom Supermercado",
+      companyName: "Aqui vai o nome da empresa",
     });
 
     var mailOptions = {
-      from: "izaquedione@gmail.com",
-      to: "izaquenunes560@gmail.com",
+      from: "wesleyleandro.dev@gmail.com",
+      to: "wesleyleandrosantos14@gmail.com",
       subject: "Test mails template",
       html,
     };
@@ -75,8 +45,6 @@ class MailsControllers {
         });
       }
     });
-
-    console.log("4");
   }
 }
 
